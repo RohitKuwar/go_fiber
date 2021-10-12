69671393797e7c44ddfd9d6b6a757b21524c5eca
RohitKuwar <rohit1kuwar@gmail.com>
2021-10-12 19:24:14 +0530

    added viper package

--- go.mod
+++ go.mod
@@ -4,14 +4,13 @@ module github.com/RohitKuwar/go_fiber
 go 1.16
 
 require (
-	cloud.google.com/go/firestore v1.5.0
+	cloud.google.com/go/firestore v1.6.0
 	cloud.google.com/go/storage v1.16.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/gofiber/fiber/v2 v2.17.0
-	github.com/joho/godotenv v1.3.0
-	github.com/valyala/fasthttp v1.29.0 // indirect
+	github.com/gofiber/fiber/v2 v2.20.1
+	github.com/joho/godotenv v1.4.0
+	github.com/spf13/viper v1.9.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.0.0-20210817190340-bfb29a6856f2 // indirect
-	google.golang.org/api v0.54.0
+	google.golang.org/api v0.56.0
 )
--- main.go
+++ main.go
@@ -4,25 +4,35 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"github.com/joho/godotenv"
+
+	"github.com/RohitKuwar/go_fiber/config"
 	"github.com/RohitKuwar/go_fiber/routes"
 	"github.com/gofiber/fiber/v2"
-	// "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
-	
-	// app.Use(logger.New())
+
+	app.Use(logger.New())
 	routes.Setup(app)
-	err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
 
-  port := os.Getenv("PORT")
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	fmt.Println("Server is runnig on port:", port)
-	app.Listen(":"+port)
-	
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+
+	port := os.Getenv("PORT")
+
+	fmt.Println("Server is runnig on port from config:", config.ServerPort)
+	fmt.Println("Server is runnig on port from dotenv:", port)
+
+	app.Listen(":" + config.ServerPort)
+
 }
